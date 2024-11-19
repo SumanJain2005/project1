db029511b8942cd225c9fa0b9fb257e7632a55b2
SumanJain2005 <sumannareshclg@gmail.com>
2024-11-19 13:37:01 +0530

    image removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import  message from "@/messages.json"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Separator } from "@/components/ui/separator"
-import image from "./SNJ.png"
+
 import {
   Carousel,
   CarouselContent,
@@ -50,7 +50,7 @@ export default function  Home() {
     <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div  className="sm:flex sm:items-center sm:justify-between">
             <a href="https://suman-jain.vercel.app/"  className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                <img src={image.src}  className="h-20 w-20" alt="Flowbite Logo" />
+                
                 <span  className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">SNJ</span>
             </a>
             <ul  className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
